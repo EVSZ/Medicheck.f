d0a7a7fcb26aaffd48693b2c2289fe00c1c46d3d
Daan Snoeijen <438919@student.fontys.nl>
2021-03-19 11:58:00 +0100

    yo

--- src/Medication/PharmaceuticalCatalogue.tsx
+++ src/Medication/PharmaceuticalCatalogue.tsx
@@ -8,17 +8,26 @@ interface PharmaceuticalCatalogueProps {
 export default function PC({ properties }: { properties: PharmaceuticalCatalogueProps }) {
 
     const medication = ["med1", "med2", "med3", "med4"];
-    const medList = medication.map((string) =>
-        <div>
+    const medList = medication.map((string, index) =>
+        <div key={index}>
             <li>{string}</li>
             <button>voeg toe</button>
         </div>
     );
 
+    const addedMedication = [""];
+    const addedMedList = addedMedication.map((string) =>
+    <div>
+        <li>{string}</li>
+    </div>
+    
+);
     return (
         <div className="list">
             <h3>Medicatie lijst</h3>
+            <input type="text" value="Zoek medicatie"></input>
+            <h3>Toegevoegde medicatie:</h3>
             <ul>{medList}</ul>
         </div>
-     );
+    );
 }
