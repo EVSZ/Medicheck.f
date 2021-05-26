955001d4fc48c53302a1f409f074da5c2859558b
Matt <mateuszmierzejek187@gmail.com>
2021-05-26 09:57:23 +0200

    begin to user profile & styleguide

--- src/NavBar/NavigationBar.tsx
+++ src/NavBar/NavigationBar.tsx
@@ -1,4 +1,5 @@
 import react from 'react';
+import {Button} from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import '../NavBar/Navbar.css';
 
@@ -12,8 +13,9 @@ export default function NavigationBar() {
             <ul>
                 <Link to="/"><a href="#Home">Home</a></Link>
                 <Link to="/Advice"><a href="#Advice">Advice</a></Link>
-                <Link to="/Registration"><a href="#User">User</a></Link>
+                <Link to="/Personal"><a href="#User">Personal</a></Link>
             </ul>
+                <Link to="/Registration"><Button variant="danger">Login</Button></Link>
         </nav>
      );
 }
