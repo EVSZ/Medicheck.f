6e7831b742ebbb3b93a223e8c785a2891ce60477
evsz <mateuszmierzejek187@gmail.com>
2021-06-02 15:19:10 +0200

    created message display template

---
+++ src/MessageDisplay.tsx
@@ -0,0 +1,34 @@
+{/* <div className="infoElement"> 
+          <div className="infoBox">
+            <div className="infoTop">
+              <h3>het advies is <strong>positief</strong></h3>
+              <FontAwesomeIcon style={{ width: '30px', height: '30px', color: '#00d050', marginLeft:'10px' }} icon={["fas", "smile-beam"]} />
+            </div>
+            <div className="infoMid">
+              <h5>Uit de informatie die wij hebben ontvangen
+              blijkt er op dit moment geen enkele reden is
+                om een afspraak te maken met uw huisarts/dokter</h5>
+            </div>
+            <div className="infoBottom">
+              <button className="btnMedium btnNormal">OK</button>
+            </div>
+          </div>
+        </div>
+        <div className="infoElement">
+          <div className="infoBox">
+            <div className="infoTop">
+              <h3>het advies is <strong>negatief</strong></h3>
+              <FontAwesomeIcon style={{ width: '30px', height: '30px', color: '#d00030', marginLeft:'10px' }} icon={["fas", "frown"]} />
+            </div>
+            <div className="infoMid">
+              <h5>Na het genereren van het advies blijkt uit de
+              resultaten dat het handig is om zo snel mogelijk
+                een afspraak te maken met uw huisarts/dokter</h5>
+            </div>
+            <div className="infoBottom">
+              <button className="btnMedium btnDanger">OK</button>
+            </div>
+          </div>
+          
+        </div> 
+    */}
