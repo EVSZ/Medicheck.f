704b7c30fc1eaf980dbfe663326df4ca14689b7d
Daan <438919@student.fontys.nl>
2021-06-02 15:35:05 +0200

    Begin E2E tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,37 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import Registration from './User/Registration/Registration';
+import { createRenderer } from 'react-dom/test-utils';
+import ReactDOM, { unmountComponentAtNode } from "react-dom";
+import { act } from '@testing-library/react';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+let container:any = null;
+
+beforeEach(() => {
+  // setup a DOM element as a render target
+  container = document.createElement("div");
+  document.body.appendChild(container);
 });
+
+afterEach(() => {
+  // cleanup on exiting
+  unmountComponentAtNode(container);
+  container.remove();
+  container = null;
+});
+
+it ('RegisterTest', () => {
+  act(() => {
+    ReactDOM.render(<Registration />, container);
+  });
+  const usernameText = container.getById('username');
+  const emailText = container.getById('email');
+  const password1Text = container.getById('password1');
+  const password2Text = container.getById('password2');
+
+  expect(usernameText.text).toBe('');
+  expect(emailText.text).toBe('');
+  expect(password1Text.text).toBe('');
+  expect(password2Text.text).toBe('');
+})
