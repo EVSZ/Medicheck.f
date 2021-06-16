483197a3e3461fb33224904be906efcbb5ed8dd3
DaanSnoeijen <438919@student.fontys.nl>
2021-06-16 13:01:16 +0200

    Revert "Merge branch 'MedicatielijstMetStyleGuide'"
    This reverts commit 20d7e403c088130f41d25bf4ebe6d5abc6704efa, reversing
    changes made to 507894d0acc28b28003e1e44ea923cd26e23a298.

--- src/Advice/GenerateAdvice.tsx
+++ src/Advice/GenerateAdvice.tsx
@@ -3,26 +3,16 @@ import axios from 'axios';
 import Medication from '../Medication/MedicationList';
 import { Link } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { useState } from 'react';
 
 
 function  AdviceGenerator()
 {
-    let AdviceResult:Boolean; 
-
     function GenerateAdvice(): void {
-        axios.get(`http://localhost:8080/algorithm/getAdvice` + localStorage.getItem('userId'))
-
-        .then((response) => {
-            alert(response.data);
-        })
-        .catch((error) => {
-            console.log(error);
-        })
-    }
+      }
 
     return (
         <body >
-            <button className="GenerateAdvice" onClick={GenerateAdvice}><Link to="/Result"></Link></button>
         </body>
     );
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,138 +1,39 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
 import NavigationBar from './NavBar/NavigationBar';
-import UserPForm from '../src/User/UserPForm';
+import UserPForm from './UserPForm';
 import MedicationList from './Medication/MedicationList';
 import AdviceGenerator from './Advice/GenerateAdvice'
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Result from './Advice/GetResultAdvice';
-import Registration from './User/Registration'
-import Styleguidetest from './Styleguidetest';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { fas } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import './Styleguide.css';
-import HealthInfo from './User/Health'
-import GetAdvice from './Advice/GetAdvice'
-import DisplayMedication from './Medication/DisplayMedication'
-import axios from 'axios';
-library.add(fas);
-
-export interface patient{
-  id: number;
-  name: string;
-  weight: number;
-  length: number;
-  pregnant: boolean;
-  birthDate: string;
-  gender: number;
-  healthInformation: healthInformation;
-  userPrescriptions: userPrescriptions[];
-}
-
-interface healthInformation{
-  clcr: number;
-  lastclcr: string;
-}
-
-interface userPrescriptions{
-  medicine: medicine;
-}
-
-interface medicine {
-  id: number;
-  name: string;
-  discription: string;
-  medicineType: number;
-  GetCurrentMedicationList: () => medicine[];
-}
+import Registration from './User/Registration/Registration'
+import Profile from './User/Profile/Profile';
 
 function App() {
-  const [patient, setPatient] = useState<patient>({id:0, name: '', weight: 0, length: 0, pregnant: false, birthDate: '0000-00-00', gender: 0, healthInformation: {clcr: 0, lastclcr: '0000-00-00'}, userPrescriptions: []});
-
-  const [id, setId] = useState<number>(patient.id);
-  const [name, setName] = useState<string>(patient.name);
-  const [weight, setWeight] = useState<number>(patient.weight);
-  const [length, setLength] = useState<number>(patient.length);
-  const [pregnant, setPregnant] = useState<boolean>(patient.pregnant);
-  const [birthDate, setBirthDate] = useState<string>(patient.birthDate);
-  const [gender, setGender] = useState<number>(patient.gender)
-
-  const [clcr, setClcr] = useState<number>(patient.healthInformation.clcr);
-  const [lastclcr, setLastclcr] = useState<string>(patient.healthInformation.lastclcr);
-  const [userPrescriptions, setUp] = useState<userPrescriptions[]>(patient.userPrescriptions);
-
-  function saveMedlist() {
-    if (patient.userPrescriptions.length === 0){ alert("Vul eerst uw medicatielijst") }
-    else{
-      let healthInformation:healthInformation = {clcr:clcr, lastclcr:lastclcr}
-        const id = localStorage.getItem('userId');
-        const payload = {id, name, weight, length, pregnant, birthDate,gender,healthInformation,userPrescriptions};
-        console.log(payload);
-        axios.put('http://localhost:8080/api/patienten/update', payload)
-        .then(res => {
-          alert('Uw gegevens zijn opgeslagen!');
-        })
-        .catch(error => {
-          alert('We konden op dit moment uw gegevens niet opslaan...');
-        })
-    }
-}
-
-function load() {
-  const payload = localStorage.getItem('id');
-  axios.post('http://localhost:8080/api/patienten/patient/'+ payload)
-  .then(res => {
-    setPatient(res.data);
-    console.log(res.data);
-    console.log(patient);
-  })
-}
-
   return (
-    
     <Router>
       <div className="App">
-        <NavigationBar /> 
+
+        <NavigationBar />
         <Switch>
           <Route path="/Result">
             <Result />
           </Route>
           <Route path="/Registration">
-            <Registration 
-            props={load} />
+            <Registration />
           </Route>
           <Route path="/Advice">
-              <div>
-                <GetAdvice />
-                <UserPForm
-                age={{age: birthDate, setAge:setBirthDate}} 
-                gender={{gender:gender, setGender:setGender}} 
-                length={{length:length, setLength:setLength}}
-                name={{name: name, setName:setName}}
-                prego={{prego:pregnant, setPrego:setPregnant}}
-                weight={{weight:weight, setWeight:setWeight}}/>
-                <HealthInfo 
-                healthinformation ={{clcr: clcr, setClcr: setClcr, lastclcr: lastclcr, setLastclcr: setLastclcr}}
-             />
-              </div>
-              <div className="right">
-                <DisplayMedication
-                iMeds={{ups: userPrescriptions, setUps: setUp}} />
-                <div className="element">
-                  <button className="btnSmall btnNormal" type="submit" onClick={saveMedlist}>Sla gegevens op</button>
-                </div>
-              </div>
-              {/* <MedicationList />
-            <AdviceGenerator /> */}
-             {/* properties={{ preg: false, gender: "", height: 155, name: "Matt", weight: 75, MedList: null }} */}
+            <UserPForm properties={{ preg: false, gender: "", height: 155, name: "Matt", weight: 75, MedList: null }} />
+            <MedicationList />
+            <AdviceGenerator />
+          </Route>
+          <Route path="/Personal">
+            <Profile/>
           </Route>
         </Switch>
       </div>
     </Router>
-    // <div className="App">
-    //   <Styleguidetest />
-    // </div>
+
   );
 }
 
