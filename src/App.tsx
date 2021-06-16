06eee690433c0792b7b62861551594e8fdb911b9
DaanSnoeijen <438919@student.fontys.nl>
2021-06-16 11:04:23 +0200

    Quick fix

--- src/App.tsx
+++ src/App.tsx
@@ -48,7 +48,7 @@ interface medicine {
 }
 
 function App() {
-  const [patient, setPatient] = useState<patient>({id:41, name: 'Matteus', weight: 69, length: 169, pregnant: true, birthDate: '2020-12-10', gender: 0, healthInformation: {clcr: 69, lastclcr: '2020-05-10'}, userPrescriptions: []});
+  const [patient, setPatient] = useState<patient>({id:0, name: '', weight: 0, length: 0, pregnant: false, birthDate: '0000-00-00', gender: 0, healthInformation: {clcr: 0, lastclcr: '0000-00-00'}, userPrescriptions: []});
 
   const [id, setId] = useState<number>(patient.id);
   const [name, setName] = useState<string>(patient.name);
